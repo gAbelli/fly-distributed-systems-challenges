fc11538820638497b8f930581c83d5492b321785
Giorgio Abelli <giorgio.abelli99@gmail.com>
2023-05-17 14:35:44 +0200

    Unique id generation

---
+++ 2-unique-id-generation/go.mod
@@ -0,0 +1,5 @@
+module maelstrom-unique-ids
+
+go 1.20
+
+require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20230516124010-52951329816e // indirect
--- 2-unique-id-generation/unique-ids.go
+++ 2-unique-id-generation/unique-ids.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
@@ -9,6 +10,8 @@ import (
 
 type Server struct {
 	n *maelstrom.Node
+
+	counter int
 }
 
 func NewServer() *Server {
@@ -17,33 +20,34 @@ func NewServer() *Server {
 	}
 }
 
-type EchoInput struct {
+type GenerateInput struct {
 	Type string `json:"type"`
-	Echo string `json:"echo"`
 }
 
-type EchoOutput struct {
+type GenerateOutput struct {
 	Type string `json:"type"`
-	Echo string `json:"echo"`
+	Id   string `json:"id"`
 }
 
-func (s *Server) echoHandler(msg maelstrom.Message) error {
-	var inputBody EchoInput
+func (s *Server) handleGenerate(msg maelstrom.Message) error {
+	var inputBody GenerateInput
 	if err := json.Unmarshal(msg.Body, &inputBody); err != nil {
 		return err
 	}
 
-	outputBody := EchoOutput{
-		Type: "echo_ok",
-		Echo: inputBody.Echo,
+	outputBody := GenerateOutput{
+		Type: "generate_ok",
+		Id:   fmt.Sprintf("%v_%d", s.n.ID(), s.counter),
 	}
+	s.counter++
+
 	return s.n.Reply(msg, outputBody)
 }
 
 func main() {
 	s := NewServer()
 
-	s.n.Handle("echo", s.echoHandler)
+	s.n.Handle("generate", s.handleGenerate)
 
 	if err := s.n.Run(); err != nil {
 		log.Fatal(err)
